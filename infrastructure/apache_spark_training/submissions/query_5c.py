49d4179e5315bbdc292947f680ef5d191ba0229e
Anju Mercian <amalphonse@users.noreply.github.com>
2023-12-26 19:51:01 -0800

    Update query_5c.py

--- infrastructure/apache_spark_training/submissions/query_5c.py
+++ infrastructure/apache_spark_training/submissions/query_5c.py
@@ -1,3 +1,8 @@
 third_sort_df = bucketedValues.repartition(10, col("event_date")) \
         .sortWithinPartitions(col("event_date")) \
         .withColumn("completion_date", col("completion_date").cast("timestamp"))
+
+
+second_sort_df = bucketedValues.repartition(10, col("completion_date")) \
+        .sortWithinPartitions(col("match_id")) \
+        .withColumn("completion_date", col("completion_date").cast("timestamp")) \

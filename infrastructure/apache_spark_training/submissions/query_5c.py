46543883ab0c16129848fcac52897676c81b266a
Anju Mercian <amalphonse@users.noreply.github.com>
2023-12-26 19:50:37 -0800

    Update query_5c.py

---
+++ infrastructure/apache_spark_training/submissions/query_5c.py
@@ -0,0 +1,3 @@
+third_sort_df = bucketedValues.repartition(10, col("event_date")) \
+        .sortWithinPartitions(col("event_date")) \
+        .withColumn("completion_date", col("completion_date").cast("timestamp"))

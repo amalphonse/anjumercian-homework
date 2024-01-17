493268ab3345987b9e74ffa2fa0b9b536e2ab35c
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-17 13:40:52 -0800

    Update query_3.sql

---
+++ analytics/applying_analytical_patterns/queries/query_3.sql
@@ -0,0 +1,15 @@
+SELECT
+ gd.player_name,
+  team_id,
+  season,
+  SUM(gd.pts)
+FROM
+  bootcamp.nba_game_details gd
+  JOIN bootcamp.nba_player_seasons s
+  ON gd.player_name=s.player_name
+GROUP BY
+  GROUPING SETS (
+    (gd.player_name, team_id),
+    (gd.player_name, season),
+    (team_abbreviation)
+  )

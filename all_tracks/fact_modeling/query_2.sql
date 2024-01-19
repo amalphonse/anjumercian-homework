95c9c9f03a085decf163d35d323fe75b2e59e7f2
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-19 11:40:01 -0800

    Update query_2.sql

---
+++ all_tracks/fact_modeling/query_2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+  anjumercian.user_devices_cumulated (
+    user_id BIGINT,
+         browser_type VARCHAR,
+    dates_active ARRAY (DATE),
+    DATE DATE
+  )
+WITH
+  (FORMAT = 'PARQUET', partitioning = ARRAY['date'])

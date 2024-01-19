be03786677315a63ecaf29ccabfd52cef3837118
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-19 14:27:56 -0800

    Update query_5b.py

--- infrastructure/apache_spark_training/submissions/query_5b.py
+++ infrastructure/apache_spark_training/submissions/query_5b.py
@@ -1,3 +1,5 @@
-second_sort_df = bucketedValues.repartition(10, col("completion_date")) \
-        .sortWithinPartitions(col("match_id")) \
-        .withColumn("completion_date", col("completion_date").cast("timestamp")) \
+second_sort_df = bucketedValues.sortWithinPartitions(col("mapid")) 
+
+second_sort_df.count()
+6885858
+

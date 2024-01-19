f91613bc2f665051e051903bd6b7058325a92ee4
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-18 17:37:42 -0800

    Update query_1.py

--- infrastructure/apache_spark_training/submissions/query_1.py
+++ infrastructure/apache_spark_training/submissions/query_1.py
@@ -1,1 +1,17 @@
 spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "-1")  #I added the line to my spark code to disable the default behavior.
+
+matches = spark.read.option("header", "true") \
+                        .option("inferSchema", "true") \
+                        .csv("/home/iceberg/data/matches.csv")
+matchDetail =  spark.read.option("header", "true") \
+                        .option("inferSchema", "true") \
+                        .csv("/home/iceberg/data/match_details.csv")
+medals = spark.read.option("header", "true") \
+                        .option("inferSchema", "true") \
+                        .csv("/home/iceberg/data/medals.csv")
+maps =  spark.read.option("header", "true") \
+                        .option("inferSchema", "true") \
+                        .csv("/home/iceberg/data/maps.csv")
+medal_matches =  spark.read.option("header", "true") \
+                        .option("inferSchema", "true") \
+                        .csv("/home/iceberg/data/medals_matches_players.csv")

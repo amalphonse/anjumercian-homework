aa8c02412bcc85783dbfa75b2c1e3e5133c344bb
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-22 19:26:30 -0800

    Update query_4.sql

--- all_tracks/dimensional_modeling/submission/query_4.sql
+++ all_tracks/dimensional_modeling/submission/query_4.sql
@@ -8,8 +8,11 @@ lagged as (
     lag(is_active, 1) over(
       partition by actor
       order by current_year) as is_active_last_year,
-quality_class,
-    current_year
+      quality_class as quality_class_this_year,
+      lag(quality_class, 1) over(
+      partition by actor
+      order by current_year) as quality_class_last_year,
+      current_year
   from anjumercian.actors
   where current_year <= 2021),
 changed as (
@@ -18,7 +21,11 @@ changed as (
     case
       when is_active_this_year <> is_active_last_year then 1
       else 0
-    end as is_changed
+    end as is_changed,
+   case
+      when quality_class_this_year <> quality_class_last_year then 1
+      else 0
+    end as quality_class_is_changed
   from lagged),
 changed_with_grps as (
   select
@@ -26,14 +33,18 @@ changed_with_grps as (
     sum(is_changed) over(
       partition by actor
       order by current_year
-    ) as grp
+    ) as grp,
+  sum(quality_class_is_changed) over(
+      partition by actor
+      order by current_year
+    ) as quality_class_grp
   from changed)
 select
   actor,
   max(is_active_this_year) as is_active,
-quality_class,
+  max(quality_class_this_year) as quality_class,
   min(current_year) as start_date,
   max(current_year) as end_date,
   2021 as current_year
 from changed_with_grps
-group by actor, grp, quality_class
+group by actor, grp, quality_class_grp

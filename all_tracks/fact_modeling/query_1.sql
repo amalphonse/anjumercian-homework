5579b07923aa798da7c16100792030a4674c9cd0
Anju Mercian <amalphonse@users.noreply.github.com>
2024-01-23 13:43:37 -0800

    Update query_1.sql

--- all_tracks/fact_modeling/query_1.sql
+++ all_tracks/fact_modeling/query_1.sql
@@ -1,56 +1,44 @@
 
 --Query 1
---I am taking yesterday's data and oining it with todays data to de dupe.
+
 
 INSERT INTO anjumercian.fct_nba_game_details
 WITH
-  yesterday AS (
-    SELECT
-      *
-    FROM
-      anjumercian.fct_nba_game_details
-    WHERE
-      game_date = '2007-04-29'
-  ),
-today AS (
-    SELECT
-      *
-    FROM
-      anjumercian.fct_nba_game_details
-    WHERE
-      game_date = '2007-04-30’'
-  )
-SELECT
-  COALESCE(t.game_id, y.game_id) AS game_id,
-  COALESCE(t.team_id, y.team_id) AS team_id,
-COALESCE(t.player_id, y.player_id) AS player_id,
-  t.dim_team_abbreviation, 
-t.dim_player_name,
- t.dim_start_position, 
-t.dim_did_not_dress, 
-t.dim_not_with_team, 
-t.m_seconds_played, 
-t.m_field_goals_made, 
-t.m_field_goals_attempted, 
-t.m_3_pointers_made, 
-t.m_3_pointers_attempted, 
-t.m_free_throws_made, 
-t.m_free_throws_attempted, 
-t.m_offensive_rebounds, 
-t.m_defensive_rebounds, 
-t.m_rebounds, 
-t.m_assists, 
-t.m_steals,
- t.m_blocks, 
-t.m_turnovers, 
-t.m_personal_fouls, 
-t.m_points, 
-t.m_plus_minus, 
-t.season, 
-t.team_did_win ,
-  '2023-08-01' AS game_date
-FROM
-  today t
- INNER JOIN yesterday y ON t.game_id = y.game_id
-  AND t.team_id = y.team_id
-AND t.player_id = y.player_id
+  cte AS (
+      SELECT
+        *,
+        ROW_NUMBER() OVER(partition by game_id, team_id, player_id ORDER BY player_id) as rnk
+      FROM
+        anjumercian.fct_nba_game_details
+      )
+  
+  select 
+      game_id,
+      team_id,
+      player_id,
+      dim_team_abbreviation, 
+      dim_player_name,
+      dim_start_position, 
+      dim_did_not_dress, 
+      dim_not_with_team, 
+      m_seconds_played, 
+      m_field_goals_made, 
+      m_field_goals_attempted, 
+      m_3_pointers_made, 
+      m_3_pointers_attempted, 
+      m_free_throws_made, 
+      m_free_throws_attempted, 
+      m_offensive_rebounds, 
+      m_defensive_rebounds, 
+      m_rebounds, 
+      m_assists, 
+      m_steals,
+      m_blocks, 
+      m_turnovers, 
+      m_personal_fouls, 
+      m_points, 
+      m_plus_minus, 
+      season, 
+      team_did_win
+  from cte
+  where rnk=1
